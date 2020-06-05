e3cac67724802d44b6a3665965dc75e30f8990a8
St. B <junkbox@email.ua>
2020-06-05 17:53:11 +0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Batman logo" src="../assets/logo.png" />
+    <HelloWorld msg="Hello #TODO#, which batman movie do you want to see?"/>
+    <MovieList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import MovieList from '@/components/MovieList.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    MovieList,
+  },
+};
+</script>
